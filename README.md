b03c7913c25aba856fdbe5f36f9b9d12a2b0f455
Shivam Jindal <shivamganeshjindal07@gmail.com>
2025-01-31 10:11:42 -0500

    Initial commit, setup basic react + vite + ts + tailwind app

--- README.md
+++
@@ -1,1 +0,0 @@
-# ShivamSingal.github.io
---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="flex h-screen items-center justify-center text-black text-3xl font-bold">
+      One Beautiful Portfolio Loading Up
+    </div>
+  );
+}
+
+export default App;
